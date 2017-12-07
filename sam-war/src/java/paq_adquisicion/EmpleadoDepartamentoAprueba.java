ae12c4dd32d875c872f892e52f7bd5e73d211e64
ModulHor <30906839+ModulHor@users.noreply.github.com>
2017-12-07 15:52:31 -0500

    nuevos

--- sam-war/src/java/paq_adquisicion/EmpleadoDepartamentoAprueba.java
+++ sam-war/src/java/paq_adquisicion/EmpleadoDepartamentoAprueba.java
@@ -11,47 +11,97 @@ import framework.componentes.Tabla;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
+import paq_adquisicion.ejb.ServiciosAdquisiones;
 import sistema.aplicacion.Pantalla;
 
 public class EmpleadoDepartamentoAprueba extends Pantalla{
     
-    private Tabla tab_emp_depar_aprue = new Tabla();
+    private Tabla tab_emp_depar = new Tabla();
+    private Tabla tab_emp_aprueba = new Tabla();
+    
+    @EJB
+    private final  ServiciosAdquisiones ser_adquisiciones = (ServiciosAdquisiones) utilitario.instanciarEJB(ServiciosAdquisiones.class);
     
     public EmpleadoDepartamentoAprueba (){
-        tab_emp_depar_aprue.setId("tab_emp_depar_aprue");   //identificador
-       tab_emp_depar_aprue.setTabla("adq_empleado_departamento", "ide_ademde", 1); 
-       tab_emp_depar_aprue.dibujar();
+       tab_emp_depar.setId("tab_emp_depar");   //identificador
+       tab_emp_depar.setTabla("adq_empleado_departamento", "ide_ademde", 1);
+       tab_emp_depar.getColumna("ide_adarad").setCombo(ser_adquisiciones.getAreaAdministrativa());
+       tab_emp_depar.getColumna("ide_ademple").setCombo(ser_adquisiciones.getEmpleado());
+       tab_emp_depar.getColumna("ide_adcarg").setCombo(ser_adquisiciones.getCargo());
+       tab_emp_depar.getColumna("ide_adtiap").setCombo(ser_adquisiciones.getTipoAprobador());
+       tab_emp_depar.agregarRelacion(tab_emp_aprueba);
+       tab_emp_depar.dibujar();
        
-       PanelTabla pat_emp_depar_aprue = new PanelTabla();
+      PanelTabla pat_emp_depar_aprue = new PanelTabla();
       pat_emp_depar_aprue.setId("pat_emp_depar_aprue");
-      pat_emp_depar_aprue.setPanelTabla(tab_emp_depar_aprue);
-      Division div_emp_depar_aprue = new Division();
-      div_emp_depar_aprue.setId("div_emp_depar_aprue");
-      div_emp_depar_aprue.dividir1(pat_emp_depar_aprue);
-      agregarComponente(div_emp_depar_aprue);
+      pat_emp_depar_aprue.setPanelTabla(tab_emp_depar);
+      
+      tab_emp_aprueba.setId("tab_emp_aprueba");   //identificador
+       tab_emp_aprueba.setTabla("adq_empleado_aprueba", "ide_ademap", 1); 
+       tab_emp_aprueba.getColumna("ide_ademple").setCombo(ser_adquisiciones.getEmpleado());
+       List lista = new ArrayList();
+       Object fila1[] = {"1", "APROBADO OFICIAL"};
+       Object fila2[] = {"2","APROBADO ENCARGADO"};
+       lista.add(fila1);
+       lista.add(fila2);
+       tab_emp_aprueba.getColumna("categoria_ademap").setCombo(lista); 
+       tab_emp_aprueba.dibujar();
+       
+      PanelTabla pat_emp_aprueba = new PanelTabla();
+      pat_emp_aprueba.setId("pat_emp_aprueba");
+      pat_emp_aprueba.setPanelTabla(tab_emp_aprueba);
+      
+      Division div_emp_aprueba = new Division();
+      div_emp_aprueba.setId("div_emp_aprueba");
+      div_emp_aprueba.dividir2(pat_emp_depar_aprue, pat_emp_aprueba, "50%","H");
+      agregarComponente(div_emp_aprueba);
     }
     @Override
     public void insertar() {
-        tab_emp_depar_aprue.insertar();
+        if(tab_emp_depar.isFocus()){
+                  tab_emp_depar.insertar();
+        }
+        else if (tab_emp_aprueba.isFocus()){
+            tab_emp_aprueba.insertar();
+        }
     }
 
     @Override
     public void guardar() {
-        tab_emp_depar_aprue.guardar();
+        if(tab_emp_depar.isFocus()){
+                  tab_emp_depar.guardar();
+        }
+        else if (tab_emp_aprueba.isFocus()){
+            tab_emp_aprueba.guardar();
+        }
         guardarPantalla();
     }
 
     @Override
     public void eliminar() {
-       tab_emp_depar_aprue.eliminar();
+       if(tab_emp_depar.isFocus()){
+                  tab_emp_depar.eliminar();
+        }
+        else if (tab_emp_aprueba.isFocus()){
+            tab_emp_aprueba.eliminar();
+        }
     }
 
-    public Tabla getTab_emp_depar_aprue() {
-        return tab_emp_depar_aprue;
+    public Tabla getTab_emp_depar() {
+        return tab_emp_depar;
     }
 
-    public void setTab_emp_depar_aprue(Tabla tab_emp_depar_aprue) {
-        this.tab_emp_depar_aprue = tab_emp_depar_aprue;
+    public void setTab_emp_depar(Tabla tab_emp_depar) {
+        this.tab_emp_depar = tab_emp_depar;
     }
+
+    public Tabla getTab_emp_aprueba() {
+        return tab_emp_aprueba;
+    }
+
+    public void setTab_emp_aprueba(Tabla tab_emp_aprueba) {
+        this.tab_emp_aprueba = tab_emp_aprueba;
+    }
+
     
 }
